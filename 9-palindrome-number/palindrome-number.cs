82389993ce05dcad454fff0f996e3a9da0343470
Michal-Hildebrandt <84286254+Michal-Hildebrandt@users.noreply.github.com>
2023-09-17 12:16:19 +0200

    Time: 34 ms (80.80%) | Memory: 29.4 MB (81.41%) - LeetSync

--- 9-palindrome-number/palindrome-number.cs
+++ 9-palindrome-number/palindrome-number.cs
@@ -1,16 +1,18 @@
 public class Solution {
     public bool IsPalindrome(int x) {
 
-        bool isPalindrome = true;
-        char[] number = x.ToString().ToCharArray();
-        char[] invertedNumber = new char[number.Length];
-        for(int i = 0; i < number.Length; i++){
-            invertedNumber[i] = number[number.Length-1-i];
-             if( invertedNumber[i] != number[i]){
-                isPalindrome = false;
-                break;
-            }
+        if(x <0 ){
+            return false;
         }
-        return isPalindrome;
+        int num = x;
+        int invertedNum = 0;
+        int digit = 0;
+        while (num != 0){
+            digit = num % 10;
+            invertedNum = invertedNum*10 + digit;
+            num /= 10;
+
+        }
+        return invertedNum == x;
     }
 }
